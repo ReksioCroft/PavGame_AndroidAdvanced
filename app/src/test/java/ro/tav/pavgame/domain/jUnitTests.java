79133becd93eab7d240a68847e9ef40dab7b749f
reksio <octavian-florin.staicu@my.fmi.unibuc.ro>
2021-02-13 14:55:48 +0200

    junit tests in progress

--- app/src/test/java/ro/tav/pavgame/domain/jUnitTests.java
+++ app/src/test/java/ro/tav/pavgame/domain/jUnitTests.java
@@ -2,26 +2,23 @@ package ro.tav.pavgame.domain;
 
 import android.content.Context;
 
-import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.List;
 
 import ro.tav.pavgame.data.GameEntity;
-import ro.tav.pavgame.data.localDB.AppDatabase;
-import ro.tav.pavgame.data.localDB.GameDao;
 
-//@RunWith( RobolectricTestRunner.class )
+@RunWith( RobolectricTestRunner.class )
 
-@RunWith( AndroidJUnit4.class )
+//@RunWith( AndroidJUnit4.class )
 public class jUnitTests {
-    private GameDao gameDao;
-    private AppDatabase db;
+    //    private GameDao gameDao;
+//    private AppDatabase db;
     Context context;
     GameEntity mGame;
 
@@ -30,7 +27,7 @@ public class jUnitTests {
         mGame = new GameEntity();
         mGame.setNumeJucator( "myTest" );
         mGame.setResult( "Win" );
-        mGame.setGameType( "Game Type: 1024x1024" );
+        mGame.setGameType( "Game Type: 4096x4096" );
         mGame.setGameId( "" );
         context = ApplicationProvider.getApplicationContext();
     }
@@ -40,21 +37,38 @@ public class jUnitTests {
         GameRemoteRepository gameRemoteRepository = new GameRemoteRepository();
         gameRemoteRepository.insertGame( mGame );
         List < GameEntity > l = gameRemoteRepository.getAllGames();
-        Boolean ok = false;
+        boolean ok = false;
         for ( GameEntity gameEntity : l ) {
             if ( gameEntity.equals( mGame ) ) {
                 ok = true;
+                break;
             }
         }
         assert ok;
     }
 
-    @Test
-    public void test2() {
-        db = Room.inMemoryDatabaseBuilder( context, AppDatabase.class ).allowMainThreadQueries().build();
-        gameDao = db.gameDao();
-        gameDao.insertGame( mGame );
-
+//    @Test
+//    public void test2() {
+//        GameUseCase gameUseCase = new GameUseCase(ApplicationProvider.getApplicationContext());
+//        gameUseCase.insertGame( mGame );
+//        GameRemoteRepository gameRemoteRepository = new GameRemoteRepository();
+//        List < GameEntity > l = gameRemoteRepository.getAllGames();
+//        boolean ok = false;
+//        for ( GameEntity gameEntity : l ) {
+//            if ( gameEntity.equals( mGame ) ) {
+//                ok = true;
+//                break;
+//            }
+//        }
+//        assert ok;
+//    }
+//
+//    @Test
+//    public void test3() {
+//        db = Room.inMemoryDatabaseBuilder( context, AppDatabase.class ).allowMainThreadQueries().build();
+//        gameDao = db.gameDao();
+//        gameDao.insertGame( mGame );
+//
 //        AppDatabase.databaseWriteExecutor.execute( new Runnable() {
 //            @Override
 //            public void run() {
@@ -63,7 +77,7 @@ public class jUnitTests {
 //        } );
 //        PavGameApplication pavGameApplication = mock(PavGameApplication.class);
 //        GameLocalRepository gameLocalRepository = new GameLocalRepository(context);
-
+//
 //        gameLocalRepository.insertGame( mGame );
 //        List < GameEntity > l = gameLocalRepository.getAllGames().getValue();
 //        try {
@@ -78,7 +92,7 @@ public class jUnitTests {
 //        } catch ( Exception e ) {
 //            assert false;
 //        }
-        assert gameDao.getSpecificGamesbyUserName( "myTest" ).getValue().isEmpty();
-    }
+//        assert gameDao.getSpecificGamesbyUserName( "myTest" ).getValue().isEmpty();
+//    }
 }
 
